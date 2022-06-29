942fbf1fd8f98e8b040badf295069b9fcda5ff6d
Thibault <thibault@mbp-de-thibault.home>
2022-06-29 17:56:36 +0200

    html form ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchMovieComponent,
   ],
   imports: [
     BrowserModule,
